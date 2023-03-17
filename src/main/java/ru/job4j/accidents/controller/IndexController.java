7edf42a25e688f64dec5d9023baf1974ff630a46
svoh86 <sms-86@mail.ru>
2023-03-17 16:20:23 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,6 +1,7 @@
 package ru.job4j.accidents.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -12,7 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("user", "admin");
         return "index";
     }
 }
