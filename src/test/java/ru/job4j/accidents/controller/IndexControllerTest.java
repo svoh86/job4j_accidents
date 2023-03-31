497e56deea53784613d0519550992c59adf43a34
svoh86 <sms-86@mail.ru>
2023-03-31 19:30:41 +0300

    0. Spring test [#6881]

---
+++ src/test/java/ru/job4j/accidents/controller/IndexControllerTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.accidents.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.job4j.accidents.Main;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest(classes = Main.class)
+@AutoConfigureMockMvc
+class IndexControllerTest {
+    /**
+     * Создает объект-заглушку. Мы можем отправлять в него запросы.
+     */
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * Аннотация @WithMockUser - Подставляет авторизованного пользователя в запрос.
+     */
+    @Test
+    @WithMockUser
+    public void index() throws Exception {
+        this.mockMvc.perform(get("/index"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+}
